666e89733a446df5ce407e1e09b6d44d4738a929
Clifford T. Matthews <ctm@devctm.com>
2016-06-22 10:52:33 -0600

    v0.14.4

--- vendor/gibbon.browser.dev.js
+++ vendor/gibbon.browser.dev.js
@@ -24,7 +24,7 @@ Parsimmon.Parser = (function() {
       index: index,
       value: value,
       furthest: -1,
-      expected: []
+      expected: ''
     };
   }
 
@@ -34,24 +34,20 @@ Parsimmon.Parser = (function() {
       index: -1,
       value: null,
       furthest: index,
-      expected: [expected]
+      expected: expected
     };
   }
 
-  function mergeReplies(result, last) {
+  function furthestBacktrackFor(result, last) {
     if (!last) return result;
-    if (result.furthest > last.furthest) return result;
-
-    var expected = (result.furthest === last.furthest)
-      ? result.expected.concat(last.expected)
-      : last.expected;
+    if (result.furthest >= last.furthest) return result;
 
     return {
       status: result.status,
       index: result.index,
       value: result.value,
       furthest: last.furthest,
-      expected: expected
+      expected: last.expected
     }
   }
 
@@ -59,27 +55,20 @@ Parsimmon.Parser = (function() {
     if (!(p instanceof Parser)) throw new Error('not a parser: '+p);
   }
 
-  function formatExpected(expected) {
-    if (expected.length === 1) return expected[0];
-
-    return 'one of ' + expected.join(', ')
-  }
-
-  function formatGot(stream, error) {
+  var formatError = Parsimmon.formatError = function(stream, error) {
+    var expected = error.expected;
     var i = error.index;
 
-    if (i === stream.length) return ', got the end of the stream'
-
+    if (i === stream.length) {
+      return 'expected ' + expected + ', got the end of the string';
+    }
 
     var prefix = (i > 0 ? "'..." : "'");
     var suffix = (stream.length - i > 12 ? "...'" : "'");
-
-    return ' at character ' + i + ', got ' + prefix + stream.slice(i, i+12) + suffix
-  }
-
-  var formatError = Parsimmon.formatError = function(stream, error) {
-    console.log('formatError', stream, error);
-    return 'expected ' + formatExpected(error.expected) + formatGot(stream, error)
+    return (
+      'expected ' + expected + ' at character ' + i + ', got ' +
+      prefix + stream.slice(i, i+12) + suffix
+    );
   };
 
   _.parse = function(stream) {
@@ -105,13 +94,13 @@ Parsimmon.Parser = (function() {
       var accum = new Array(numParsers);
 
       for (var j = 0; j < numParsers; j += 1) {
-        result = mergeReplies(parsers[j]._(stream, i), result);
+        result = furthestBacktrackFor(parsers[j]._(stream, i), result);
         if (!result.status) return result;
         accum[j] = result.value
         i = result.index;
       }
 
-      return mergeReplies(makeSuccess(i, accum), result);
+      return furthestBacktrackFor(makeSuccess(i, accum), result);
     });
   };
 
@@ -139,7 +128,7 @@ Parsimmon.Parser = (function() {
     return Parser(function(stream, i) {
       var result;
       for (var j = 0; j < parsers.length; j += 1) {
-        result = mergeReplies(parsers[j]._(stream, i), result);
+        result = furthestBacktrackFor(parsers[j]._(stream, i), result);
         if (result.status) return result;
       }
       return result;
@@ -183,14 +172,14 @@ Parsimmon.Parser = (function() {
       var prevResult;
 
       for (;;) {
-        result = mergeReplies(self._(stream, i), result);
+        result = furthestBacktrackFor(self._(stream, i), result);
 
         if (result.status) {
           i = result.index;
           accum.push(result.value);
         }
         else {
-          return mergeReplies(makeSuccess(i, accum), result);
+          return furthestBacktrackFor(makeSuccess(i, accum), result);
         }
       }
     });
@@ -228,7 +217,7 @@ Parsimmon.Parser = (function() {
 
       for (var times = 0; times < min; times += 1) {
         result = self._(stream, i);
-        prevResult = mergeReplies(result, prevResult);
+        prevResult = furthestBacktrackFor(result, prevResult);
         if (result.status) {
           i = result.index;
           accum.push(result.value);
@@ -238,7 +227,7 @@ Parsimmon.Parser = (function() {
 
       for (; times < max; times += 1) {
         result = self._(stream, i);
-        prevResult = mergeReplies(result, prevResult);
+        prevResult = furthestBacktrackFor(result, prevResult);
         if (result.status) {
           i = result.index;
           accum.push(result.value);
@@ -246,7 +235,7 @@ Parsimmon.Parser = (function() {
         else break;
       }
 
-      return mergeReplies(makeSuccess(i, accum), prevResult);
+      return furthestBacktrackFor(makeSuccess(i, accum), prevResult);
     });
   };
 
@@ -265,7 +254,7 @@ Parsimmon.Parser = (function() {
     return Parser(function(stream, i) {
       var result = self._(stream, i);
       if (!result.status) return result;
-      return mergeReplies(makeSuccess(result.index, fn(result.value)), result);
+      return furthestBacktrackFor(makeSuccess(result.index, fn(result.value)), result);
     });
   };
 
@@ -280,12 +269,7 @@ Parsimmon.Parser = (function() {
   };
 
   _.desc = function(expected) {
-    var self = this;
-    return Parser(function(stream, i) {
-      var reply = self._(stream, i);
-      if (!reply.status) reply.expected = [expected];
-      return reply;
-    });
+    return this.or(fail(expected))
   };
 
   // -*- primitive parsers -*- //
@@ -307,7 +291,6 @@ Parsimmon.Parser = (function() {
 
   var regex = Parsimmon.regex = function(re, group) {
     var anchored = RegExp('^(?:'+re.source+')', (''+re).slice((''+re).lastIndexOf('/')+1));
-    var expected = '' + re;
     if (group == null) group = 0;
 
     return Parser(function(stream, i) {
@@ -319,7 +302,7 @@ Parsimmon.Parser = (function() {
         if (groupMatch != null) return makeSuccess(i+fullMatch.length, groupMatch);
       }
 
-      return makeFailure(i, expected);
+      return makeFailure(i, re);
     });
   };
 
@@ -424,7 +407,7 @@ Parsimmon.Parser = (function() {
       var result = self._(stream, i);
       if (!result.status) return result;
       var nextParser = f(result.value);
-      return mergeReplies(nextParser._(stream, result.index), result);
+      return furthestBacktrackFor(nextParser._(stream, result.index), result);
     });
   };
 
